e0d327da3bcc192f93f5cfe8a4946568e8974930
ZeNGrooT1 <iamsuhaskg18@gmail.com>
2026-03-19 17:28:39 +0530

    Seo_Commit_2

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "next-themes";
 import App from "./App.tsx";
-import appStyles from "./index.css?inline";
-
-const INLINE_STYLE_ID = "mailcraft-inline-styles";
-
-if (!document.getElementById(INLINE_STYLE_ID)) {
-  const styleTag = document.createElement("style");
-  styleTag.id = INLINE_STYLE_ID;
-  styleTag.textContent = appStyles;
-  document.head.appendChild(styleTag);
-}
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider attribute="class" defaultTheme="system" storageKey="mailcraft-theme">
