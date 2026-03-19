8a4afb8b83b429131f4b060140e00799f4dd2de8
ZeNGrooT1 <iamsuhaskg18@gmail.com>
2026-03-19 17:07:46 +0530

    Seo_commit_1

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "next-themes";
 import App from "./App.tsx";
-
-void import("./index.css");
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider attribute="class" defaultTheme="system" storageKey="mailcraft-theme">
